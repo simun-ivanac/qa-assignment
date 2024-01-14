d2019b135145535c41dcaffbf98e5deca345192a
simun-ivanac <ivanac.simun@gmail.com>
2024-01-14 19:08:31 +0100

    display user messages on frontend

--- wp-content/themes/qa-assignment/src/Session/SessionTokenStorage.php
+++ wp-content/themes/qa-assignment/src/Session/SessionTokenStorage.php
@@ -54,4 +54,63 @@ class SessionTokenStorage
 		\wp_redirect('/');
 		exit;
 	}
+
+	/**
+	 * Check whether token is active or expired. Remove from session if expired.
+	 *
+	 * @return bool
+	 */
+	public static function isTokenActive(): bool
+	{
+		if (
+			!isset($_SESSION['qss-token']['key']) ||
+			!isset($_SESSION['qss-token']['expiration'])
+		) {
+			return false;
+		}
+
+		$sessionDateTime = $_SESSION['qss-token']['expiration']['datetime'] ?? '';
+		$sessionTimezone = $_SESSION['qss-token']['expiration']['timezone'] ?? self::TOKEN_TIMEZONE;
+
+		$currentDateTime = new DateTime('now', new DateTimeZone($sessionTimezone));
+		$currentDateTime->format('Y-m-d H:i:s');
+
+		$expirationDateTime = new DateTime($sessionDateTime, new DateTimeZone($sessionTimezone));
+		$expirationDateTime->format('Y-m-d H:i:s');
+
+		// Still active.
+		if ($expirationDateTime > $currentDateTime) {
+			return true;
+		}
+
+		// If expired, remove token from session (just to be sure...).
+		unset($_SESSION['qss-token']);
+
+		return false;
+	}
+
+	/**
+	 * Format token expiration time to be in human readable mode.
+	 *
+	 * @return string
+	 */
+	public static function getTokenDuration(): string
+	{
+		$sessionDateTime = $_SESSION['qss-token']['expiration']['datetime'] ?? '';
+		$sessionTimezone = $_SESSION['qss-token']['expiration']['timezone'] ?? self::TOKEN_TIMEZONE;
+
+		if ($sessionDateTime === '') {
+			return '0min 0sec';
+		}
+
+		$currentDateTime = new DateTime('now', new DateTimeZone($sessionTimezone));
+		$currentDateTime->format('Y-m-d H:i:s');
+
+		$expirationDateTime = new DateTime($sessionDateTime, new DateTimeZone($sessionTimezone));
+		$expirationDateTime->format('Y-m-d H:i:s');
+
+		$interval = $currentDateTime->diff($expirationDateTime);
+
+		return $interval->format('%imin %ssec');
+	}
 }
